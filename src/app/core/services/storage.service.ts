9debe5b7d82511b837703f1532335db3a6a4207c
Maxime Gravouil <maxgravo@wkmfr7571087.global.publicisgroupe.net>
2019-10-17 16:20:11 +0200

    add prefix to storage service

--- src/app/core/services/storage.service.ts
+++ src/app/core/services/storage.service.ts
@@ -3,17 +3,19 @@ import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 
 @Injectable()
 export class StorageService {
+  private prefix = 'hpa-';
+
   constructor(@Inject(PLATFORM_ID) private platformId) {}
 
   public set(key: string, value: any) {
     if (isPlatformBrowser(this.platformId)) {
-      localStorage.setItem(key, JSON.stringify(value));
+      localStorage.setItem(this.prefix + key, JSON.stringify(value));
     }
   }
 
   public get<T>(key: string): T {
     if (isPlatformBrowser(this.platformId)) {
-      return JSON.parse(localStorage.getItem(key)) as T;
+      return JSON.parse(localStorage.getItem(this.prefix + key)) as T;
     } else {
       return null;
     }
@@ -21,7 +23,7 @@ export class StorageService {
 
   public unset(key: string) {
     if (isPlatformBrowser(this.platformId)) {
-      localStorage.removeItem(key);
+      localStorage.removeItem(this.prefix + key);
     }
   }
 
